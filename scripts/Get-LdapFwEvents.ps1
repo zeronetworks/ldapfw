914024aa4fb15ed1559579dbebaab0c15334c538
dekelpaz <dekel@zeronetworks.com>
2024-08-22 05:45:11 -0700

    powershell log dump script improvements

--- scripts/Get-LdapFwEvents.ps1
+++ scripts/Get-LdapFwEvents.ps1
@@ -16,9 +16,20 @@ function Parse-LDAPFWLog {
         default { $EventID }
     }
 
-    $DN = [regex]::Match($Event.Message, 'DN:\s*(.*?)(?=\r|\n)')
+    $DN = ""
+    $NewDN = ""
 
-    $Attributes = [regex]::Match($Event.Message, 'Attributes:(.\t*)(.*[^\r|\n|\t])').Groups[2].Value
+    $DNMatches = [regex]::Matches($Event.Message, 'DN:(.\t*)(.*[^\r|\n|\t])')
+
+    if ($DNMatches.Count -gt 0) {
+        $DN = $DNMatches[0].Groups[2].Value
+    }
+
+    if ($DNMatches.Count -gt 1) {
+        $NewDN = $DNMatches[1].Groups[2].Value
+    }
+
+    $Attributes = [regex]::Match($Event.Message, 'Attribute[s]?:(.\t*)(.*[^\r|\n|\t])').Groups[2].Value
 
     if ([string]::IsNullOrWhiteSpace($Attributes)) {
         $Attributes = ""
@@ -41,26 +52,32 @@ function Parse-LDAPFWLog {
     if ([string]::IsNullOrWhiteSpace($Data)) {
         $Data = ""
     }
+	
+    $Oid = [regex]::Match($Event.Message, 'Oid:(.\t*)(.*[^\r|\n|\t])').Groups[2].Value
 
-    return New-Object PSObject -Property @{
-        'Log Source' = 'LDAPFW'
-        'Event Type' = $EventType
-        'TimeCreated' = $Event.TimeCreated
-        'Security ID' = [regex]::Match($Event.Message, 'Security ID:\s*(.*?)($|\r\n)').Groups[1].Value
-        'Action' = [regex]::Match($Event.Message, 'Action:\s*(.*?)($|\r\n)').Groups[1].Value
-        'DN' = $DN.Groups[1].Value
-        'Filter' = [regex]::Match($Event.Message, 'Filter:\s*(.*?)(?=\r|\n)').Groups[1].Value
-        'Scope' = [regex]::Match($Event.Message, 'Scope:\s*(.*?)($|\r\n)').Groups[1].Value
-        'Attributes' = $Attributes
-        'Entry List' = $EntryList
-        'Value' = $Value
-        'New DN' = $DN.Groups[2].Value
-        'Delete Old' = [regex]::Match($Event.Message, 'Delete Old:\s*(.*?)($|\r\n)').Groups[1].Value
-        'Oid' = [regex]::Match($Event.Message, 'Oid:\s*(.*?)($|\r\n)').Groups[1].Value
-        'Data' = $Data
-        'Client Network Address' = [regex]::Match($Event.Message, 'Client Network Address:\s*([\d\.:a-zU]+)').Groups[1].Value
-        'Client Port' = [regex]::Match($Event.Message, 'Client Port:\s*(.*?)($|\r\n)').Groups[1].Value
+    if ([string]::IsNullOrWhiteSpace($Oid)) {
+        $Oid = ""
     }
+
+    return New-Object PSObject -Property $([ordered]@{
+        LogSource = 'LDAPFW'
+        TimeCreated = $Event.TimeCreated
+        EventType = $EventType
+        SecurityID = [regex]::Match($Event.Message, 'Security ID:\s*(.*?)($|\r\n)').Groups[1].Value
+        ClientNetworkAddress = [regex]::Match($Event.Message, 'Client Network Address:\s*([\d\.:a-zU]+)').Groups[1].Value
+        ClientPort = [regex]::Match($Event.Message, 'Client Port:\s*(.*?)($|\r\n)').Groups[1].Value
+        Action = [regex]::Match($Event.Message, 'Action:\s*(.*?)($|\r\n)').Groups[1].Value
+        DN = $DN
+        SearchFilter = [regex]::Match($Event.Message, 'Filter:\s*(.*?)(?=\r|\n)').Groups[1].Value
+        Scope = [regex]::Match($Event.Message, 'Scope:\s*(.*?)($|\r\n)').Groups[1].Value
+        Attributes = $Attributes
+        EntryList = $EntryList
+        Value = $Value
+        NewDN = $NewDN
+        DeleteOld = [regex]::Match($Event.Message, 'Delete Old:\s*(.*?)($|\r\n)').Groups[1].Value
+        ExtendedOid = $Oid
+        ExtendedData = $Data
+    })
 }
 
 # Retrieve and parse LDAPFW events
